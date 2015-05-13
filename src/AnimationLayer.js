32a8ab046cbdcdf3fda2433550cd7f3843d6fc76
gawao <gawao57@gmail.com>
2015-05-13 21:24:20 +0900

    Cocos2d-JS Tutorial 6: Add Chipmunk Physic Engine to Our Game

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -2,8 +2,13 @@ var AnimationLayer = cc.Layer.extend({
 	spriteSheet: null,
 	runningAction: null,
 	sprite: null,
-	ctor: function() {
+	space: null,
+	body: null,
+	shape: null,
+	
+	ctor: function(space) {
 		this._super();
+		this.space = space;
 		this.init();
 	},
 	init: function() {
@@ -27,8 +32,22 @@ var AnimationLayer = cc.Layer.extend({
 		// 4. wrap the animate action with a repeat forever action
 		this.runningAction = new cc.RepeatForever(new cc.Animate(animation));
 		
-		this.sprite = new cc.Sprite("#runner0.png");
-		this.sprite.attr({x: 80, y: 85});
+		// 1. create PhysicsSprite with a sprite frame name
+		this.sprite = new cc.PhysicsSprite("#runner0.png");
+		var contentSize = this.sprite.getContentSize();
+		// 2. init the runner physic body
+		this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width, contentSize.height));
+		// 4. apply impulse to the body
+		this.body.applyImpulse(cp.v(150, 0), cp.v(0, 0)); // run speed
+		// 5. add the created body to space
+		this.space.addBody(this.body);
+		// 6. create the shape for the body
+		this.shape = new cp.BoxShape(this.body, contentSize.width - 14, contentSize.height);
+		// 7. add shape to space
+		this.space.addShape(this.shape);
+		// 8. set body to the physic sprite
+		this.sprite.setBody(this.body);
+		
 		this.sprite.runAction(this.runningAction);
 		
 		this.spriteSheet.addChild(this.sprite);
